8eaf2c1adc8c5010a27248a6a0eb2234d48fef92
Juan Morales <juan.morales@inworx.com>
2024-01-16 12:34:35 -0300

    commit game

--- README.md
+++ README.md
@@ -1,8 +1,4 @@
-# Getting Started with Create React App
-
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
-
-## Available Scripts
+# Juego memory card con caras de Messi
 
 In the project directory, you can run:
 
--- src/App.js
+++ src/App.js
@@ -1,23 +1,108 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import Card from './components/Card';
+import { images } from './import';
+import './app.css';
+import Congratulations from './components/Congratulations';
 
 function App() {
+  const [cards, setCards] = useState([]);
+  const [firstCard, setFirstCard] = useState({});
+  const [secondCard, setSecondCard] = useState({});
+  const [disabledCards, setDisabledCards] = useState([]);
+  const [unFlippedCards, setUnflippedCards] = useState([]);
+  const [finish, setFinish] = useState(false);
+
+  const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      let temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+  };
+
+  function flipCard(name, number) {
+    if (
+      (firstCard.name === name && firstCard.number === number) ||
+      (secondCard.name === name && secondCard.number === number) ||
+      disabledCards?.includes(number)
+    ) {
+      return 0;
+    }
+    if (!firstCard.name) {
+      setFirstCard({ name, number });
+    } else if (!secondCard.name) {
+      setSecondCard({ name, number });
+    }
+    return 1;
+  }
+
+  function checksForMatch() {
+    if (firstCard.name && secondCard.name) {
+      const idems = firstCard.name === secondCard.name;
+      idems ? disableCards() : unflipCards();
+    }
+  }
+
+  function disableCards() {
+    setDisabledCards([...disabledCards, firstCard.number, secondCard.number]);
+    if(disabledCards?.length === 10){
+      setFinish(true);
+    }
+    resetCards();
+  }
+  function unflipCards() {
+    setUnflippedCards([firstCard.number, secondCard.number]);
+    resetCards();
+  }
+
+  function resetCards() {
+    setFirstCard({});
+    setSecondCard({});
+  }
+
+  function restart() {
+    setCards([]);
+    setFirstCard({});
+    setSecondCard({});
+    setDisabledCards([]);
+    setUnflippedCards([]);
+    setFinish(false);
+    shuffleArray(images);
+    setCards(images);
+  }
+
+  function flipAllCards() {
+    setCards((cards) => cards.map((card) => ({ ...card, flipped: false })));
+  }
+
+  useEffect(() => {
+    shuffleArray(images);
+    setCards(images);
+  }, []);
+
+  useEffect(() => {
+    if (secondCard?.name) {
+      checksForMatch();
+    }
+  }, [secondCard]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="cards-container">
+        {cards.map((card, index) => (
+          <Card
+            key={index}
+            name={card.player}
+            number={index}
+            frontFace={card.src}
+            flipCard={flipCard}
+            unFlippedCards={unFlippedCards}
+            finish = {finish}
+          />
+        ))}
+      </div>
+      <Congratulations restart={restart} finish = {finish}/>
     </div>
   );
 }
